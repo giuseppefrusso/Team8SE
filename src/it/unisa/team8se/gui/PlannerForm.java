3b05242cf9abc0b17b2cda1016af34113f10c2be
Cr4zyRi0s <Cr4zyRi0s.channel@gmail.com>
2020-12-03 16:21:53 +0100

    Update PlannerForm.java

--- src/it/unisa/team8se/gui/PlannerForm.java
+++ src/it/unisa/team8se/gui/PlannerForm.java
@@ -11,6 +11,7 @@ import it.unisa.team8se.gui.datamodels.ActivityTableDataModel;
 import it.unisa.team8se.gui.datamodels.MaintainerAvailabilityDataModel;
 import it.unisa.team8se.models.Activity;
 import it.unisa.team8se.models.Maintainer;
+import java.awt.Toolkit;
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -21,9 +22,11 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
-import javax.swing.text.DocumentFilter;
+import javax.swing.text.Element;
+import javax.swing.text.PlainDocument;
 
 /**
  *
@@ -42,14 +45,33 @@ public class PlannerForm extends javax.swing.JFrame {
         setupActivityTable();
         setupMaintainerTable();
         setupTextBoxes();
-        
-        
 
         switchToActivityList();
     }
-    
-    private void setupTextBoxes(){
-        
+
+    private void setupTextBoxes() {
+        /*
+        interventionDescText.setDocument(new PlainDocument(){
+            private final int MAX_LENGTH = 50;
+            
+            @Override
+            public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+                int texLen = interventionDescText.getText().length();
+                int chngLen = str.length();
+                if (texLen + chngLen <= MAX_LENGTH){
+                     super.insertString(offs, str, a);
+                }
+                else{
+                    int diff = MAX_LENGTH - texLen;
+                    if(diff > 0){
+                        String s = str.substring(0, diff);
+                        super.insertString(offs, s, a);
+                    }
+                    Toolkit.getDefaultToolkit().beep();
+                }
+                System.out.println(texLen);
+            }
+        });*/
         
         interventionDescText.setEditable(false);
         workspaceNotesText.setEditable(false);
@@ -57,7 +79,6 @@ public class PlannerForm extends javax.swing.JFrame {
 
     private void setupActivityTable() {
         activities = new LinkedList<>();
-        Collections.addAll(activities, Activity.getAllDatabaseInstances());
         activityTable.setModel(new ActivityTableDataModel(activities));
         ListSelectionModel selectionModel = activityTable.getSelectionModel();
         selectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -487,6 +508,7 @@ public class PlannerForm extends javax.swing.JFrame {
     }
 
     private void switchToActivityList() {
+        refreshActivities();
         tabbedPane.setEnabledAt(1, false);
         tabbedPane.setEnabledAt(2, false);
         selectedActivity = null;
@@ -503,6 +525,7 @@ public class PlannerForm extends javax.swing.JFrame {
     }
 
     private void switchToMaintainerList() {
+        refreshMaintainers();
     }
 
 
@@ -549,8 +572,10 @@ public class PlannerForm extends javax.swing.JFrame {
     }//GEN-LAST:event_maintainerListButtonActionPerformed
 
     private void interventionDescTextFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_interventionDescTextFocusLost
-        if(selectedActivity == null) return;
-        
+        if (selectedActivity == null) {
+            return;
+        }
+
         interventionDescText.setEditable(false);
         interventionDescDoneButton.setEnabled(false);
         selectedActivity.setInterventionDescription(interventionDescText.getText());
@@ -558,8 +583,10 @@ public class PlannerForm extends javax.swing.JFrame {
     }//GEN-LAST:event_interventionDescTextFocusLost
 
     private void workspaceNotesTextFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_workspaceNotesTextFocusLost
-        if(selectedActivity == null) return;
-        
+        if (selectedActivity == null) {
+            return;
+        }
+
         workspaceNotesText.setEditable(false);
         workspaceNotesDoneButton.setEnabled(false);
         selectedActivity.setWorkspaceNotes(workspaceNotesText.getText());
@@ -580,7 +607,24 @@ public class PlannerForm extends javax.swing.JFrame {
         selectedActivity.updateWorkspaceNotesInDatabase();
     }//GEN-LAST:event_workspaceNotesDoneButtonActionPerformed
 
-    
+    private void refreshMaintainers() {
+        try {
+            Maintainer[] m = Maintainer.getAllDatabaseInstances();
+            if (m != null) {
+                Collections.addAll(maintainers, m);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PlannerForm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void refreshActivities() {
+        Activity[] a = Activity.getAllDatabaseInstances();
+        if (a != null) {
+            Collections.addAll(activities,a);
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
